c1dd166dd321834fb770a3880e8b646eaeeb677b
pavel444-byte <pavel.melnikov.kid.1993@gmail.com>
2025-08-12 09:38:58 +0500

    refactor(install_dependencies): remove installation script for dependencies

--- install_depencies.sh
+++
@@ -1,5 +0,0 @@
-echo "Preparing... (Warning: if you start this operation without venv by uv or other please cancel it and create venv right no)"
-echo "Warning: Do not create venv by pyenv or anaconda use uv. Starting..."
-uv pip install openai streamlit dotenv typing
-echo "All installed"
-exit
