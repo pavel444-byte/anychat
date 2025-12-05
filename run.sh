b1915c9e726bc330cdfddeebbbe527c59d278463
pavel444-byte <pavel.melnikov.kid.1993@gmail.com>
2025-12-05 16:24:06 +0500

    feat(run-script): add initial run script to launch the application with countdown

---
+++ run.sh
@@ -0,0 +1,27 @@
+echo "running the application..."
+sleep 1
+echo "Loading .env"
+sleep 1
+echo "10"
+sleep 1
+echo "9"
+sleep 1
+echo "8"
+sleep 1
+echo "7"
+sleep 1
+echo "6"
+sleep 1
+echo "5"
+sleep 1
+echo "4"
+sleep 1
+echo "3"
+sleep 1
+echo "2"
+sleep 1
+echo "1"
+sleep 1
+echo "0"
+echo "Launching application..."
+streamlit run main.py
